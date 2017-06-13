f3557adb3a9384093b1dd645f8713e99275ebc5e
beebeean09 <beebeean09@gmail.com>
2017-06-12 17:48:47 -0700

    todo list add component

---
+++ frontend/components/todo/todo_list_item.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class TodoListItem extends React.Compnent {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    const todo = this.props.todo;
+    
+    return(
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default TodoListItem;
